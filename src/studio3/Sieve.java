6df3aaceed4c701a7fc0614bdbde6be8cb57a626
Andy Lin <anji@wustl.edu>
2025-09-17 14:41:53 -0500

    message

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Sieve {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("what number do you want to choose?");
+        int n = 
+
+    }
+}
