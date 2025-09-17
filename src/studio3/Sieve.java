c44f0a691ef23ec23611322ae23ce4b05c45e2bb
R-Past <p.rex@wustl.edu>
2025-09-17 15:30:27 -0500

    Push

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -5,7 +5,25 @@ public class Sieve {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("what number do you want to choose?");
-        int n = 
+        int n = scan.nextInt();
+        boolean[] prime = new boolean[n];
 
+        for (int i = 1; i <= prime.length; i++){
+            prime[i-1] = true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++){
+            int test = i+i;
+            while (test <= n){
+                prime[test-1] = false;
+                test = test + i;
+            }
+        }
+
+        for (int i = 1; i <= prime.length; i++){
+            if(prime[i-1]){
+                System.out.println(i);
+        }
     }
+}
 }
